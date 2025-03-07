92233f9f2b5d9e861d7a9062a8e21d0e6c78d001
Sayan Dutta <rockistuart@gmail.com>
2025-03-07 08:20:52 +0530

    Add Pizza component to display pizza options in the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+const Pizza = () =>
+	React.createElement("ul", {}, [
+		React.createElement("li", {}, "Cheese"),
+		React.createElement("li", {}, "Pepperoni"),
+		React.createElement("li", {}, "Sausage"),
+	]);
+
 const App = () => {
-	return React.createElement(
-		"div",
-		{},
-		React.createElement("h1", {}, "Padre Gino's")
-	);
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Padre Gino's"),
+		React.createElement(Pizza),
+	]);
 };
 
 const container = document.getElementById("root");
